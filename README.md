8ec08fccb3e9b2636bcad84fa57276eb321c0972
leonW7 <3203362182@qq.com>
2019-05-03 21:34:12 +0800

    add unluac for LuaQ file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Renamefile
 rename file according to its fileformat
+
+## add unzip 
+
+7z fileformat
+
+## add unluac
+
+luaQ、luaR、luaS 分别代表 Lua 5.1、5.2、5.3 的 precompiled chunk。Q = 0x51、R = 0x52、S = 0x53
+
--- rename.py
+++ rename.py
@@ -2,6 +2,38 @@ import os
 
 # path = "data\\UBI" #test data
 path = "E:\\firmware\\DSR\\_DSR-500_Bx_FW3.12B101H_WW.extracted\\extracted\\1086010322\\UBI"
+#path = "E:\\IoT\\dlink\\DSR\\bin"
+
+def unzip7z(filepath,dirpath):
+    if ".7z" not in filepath:
+        new_path = filepath + ".7z"
+        try:
+            os.rename(filepath, new_path)
+        except OSError as e:
+            print('Reason:', e)
+            return False
+    else:
+        new_path = filepath
+    print(new_path)
+    try:
+        os.system("7z x " + new_path + " -y -o" + dirpath)
+        print("Unzip success to %s ..." % dirpath)
+    except OSError as e:
+        print("Unzip failed to %s ..." % dirpath)
+        print('Reason:', e)
+        return False
+    return True
+
+def unluac(filepath,dirpath):
+    try:
+        os.system("java -jar unluac_2015_06_13.jar " + filepath + " > " + dirpath + "tmp.lua")
+        os.remove(filepath)
+        os.rename(dirpath + "tmp.lua", filepath)
+    except OSError as e:
+        print("Unluac failed to %s ..." % dirpath)
+        print('Reason:', e)
+        return False
+    return True
 
 if __name__ == "__main__":
     dirfile = os.walk(path)
@@ -13,26 +45,29 @@ if __name__ == "__main__":
             try:
                 with open(fullpath, "rb") as file:
                     line = file.readline()
-                    print(line)
                     if b'\x37\x7A\x58\x5A' in line:
                         print("find 7z file")
                         file.close()
-                        if ".7z" not in fullpath:
-                            new_path = fullpath + ".7z"
-                            try:
-                                os.rename(fullpath, new_path)
-                            except OSError as e:
-                                print('Reason:', e)
-                        else:
-                            new_path = fullpath
-                        print(new_path)
-                        try:
-                            os.system("7z x " + new_path + " -y -o" + dirpath)
-                            print("Unzip success to %s ..." % dirpath)
-                        except OSError as e:
-                            print("Unzip failed to %s ..." % dirpath)
-                            print('Reason:', e)
+                        unzip7z(fullpath, dirpath)
                     else:
                         print("not 7z file")
+            except IOError:
+                print("open file error!!!\n")
+
+    dirfile = os.walk(path)
+
+    for dirpath, dirnames, filenames in dirfile:
+        for filename in filenames:
+            fullpath = os.path.join(dirpath, filename)
+            print(fullpath)
+            try:
+                with open(fullpath, "rb") as file:
+                    line = file.readline()
+                    if b'\x4C\x75\x61\x51' in line:
+                        print("find LuaQ file")
+                        file.close()
+                        unluac(fullpath, dirpath)
+                    else:
+                        print("not LuaQ file")
             except IOError:
                 print("open file error!!!\n")
